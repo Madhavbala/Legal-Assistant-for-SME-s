c01c500974a2c2c815ddc7f75d5359d933cc38e5
Madhavan BD <103066628+Madhavbala@users.noreply.github.com>
2026-02-04 15:56:04 +0530

    Update ip_rules.py

--- core/ip_rules.py
+++ core/ip_rules.py
@@ -1,7 +1,9 @@
 def is_ip_clause(clause: str) -> bool:
-    keywords = [
+keywords = [
     "intellectual property", "ownership", "exclusive", "assign", 
-    "no rights to reuse", "no rights to modify", "license", "patent", "copyright"]
+    "no rights to reuse", "no rights to modify", "license", "patent", "copyright"
+]
+
     clause_lower = clause.lower()
     return any(k.lower() in clause_lower for k in keywords)
 
